f19e24d011b0ad7cad5bededdb033768fa3e4990
Rodolfo Win <rndcsjj@gmail.com>
2023-10-01 21:24:30 -0300

    feat: adiciona estrutura de periodo e nota

--- app/Providers/RepositoryServiceProvider.php
+++ app/Providers/RepositoryServiceProvider.php
@@ -5,9 +5,13 @@ namespace App\Providers;
 use App\Domain\AlunoDisciplina\Contracts\AlunoDisciplinaInterface;
 use App\Domain\Alunos\Contracts\AlunoInterface;
 use App\Domain\Disciplinas\Contracts\DisciplinaInterface;
+use App\Domain\Notas\Contracts\NotaInterface;
 use App\Infra\Repositories\AlunoDisciplinaRepository;
 use App\Infra\Repositories\AlunoRepository;
 use App\Infra\Repositories\DisciplinaRepository;
+use App\Domain\Periodos\Contracts\PeriodoInterface;
+use App\Infra\Repositories\NotaRepository;
+use App\Infra\Repositories\PeriodoRepository;
 use Illuminate\Support\ServiceProvider;
 
 class RepositoryServiceProvider extends ServiceProvider
@@ -20,6 +24,8 @@ class RepositoryServiceProvider extends ServiceProvider
         $this->app->bind(AlunoInterface::class, AlunoRepository::class);
         $this->app->bind(DisciplinaInterface::class, DisciplinaRepository::class);
         $this->app->bind(AlunoDisciplinaInterface::class, AlunoDisciplinaRepository::class);
+        $this->app->bind(PeriodoInterface::class, PeriodoRepository::class);
+        $this->app->bind(NotaInterface::class, NotaRepository::class);
     }
 
     /**
