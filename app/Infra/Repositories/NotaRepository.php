2eead191c3b7d37c5199f3da4f8ec1f58abce3d5
Rodolfo Win <rndcsjj@gmail.com>
2023-10-06 07:55:45 -0300

    feat: cria nota repository

---
+++ app/Infra/Repositories/NotaRepository.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Infra\Repositories;
+
+use App\Domain\Notas\Contracts\NotaInterface;
+use App\Models\Nota;
+use Illuminate\Http\Request;
+
+class NotaRepository implements NotaInterface
+{
+  protected $model;
+
+  public function __construct(Nota $nota)
+  {
+    $this->model = $nota;
+  }
+
+  public function store(Request $request): Nota
+  {
+    $nota = Nota::create($request->all());
+    return $nota;
+  }
+
+  public function getNotasByAlunoDisciplina(int $id): Nota
+  {
+    $notas = Nota::with('AlunoDisciplina')
+      ->find($id);
+
+    return $notas;
+  }
+}
