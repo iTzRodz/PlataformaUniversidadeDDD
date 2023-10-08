2ec72934698bc4349240d3935c7fa1f0d1688d29
Rodolfo <rndcsjj@gmail.com>
2023-10-08 14:28:39 -0300

    feat: adiciona AlunoRequest na funcao de save

--- app/Domain/Alunos/Contracts/AlunoInterface.php
+++ app/Domain/Alunos/Contracts/AlunoInterface.php
@@ -3,6 +3,7 @@
 
 namespace App\Domain\Alunos\Contracts;
 
+use App\Http\Requests\AlunoRequest;
 use App\Models\Aluno;
 use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Database\Eloquent\Model;
@@ -11,8 +12,8 @@ use Illuminate\Http\Request;
 interface AlunoInterface
 {
   public function getAlunos() : Collection;
-  public function getAlunosById(int $id): ?Model;
-  public function insertAluno(Request $request);
+  public function getAlunosById(int $id): ?Aluno;
+  public function insertAluno(AlunoRequest $request);
   public function update(Aluno $aluno, Request $body);
   public function delete(Aluno $aluno);
 }
