cdecdfce84af602a58f50179222a02a35dd31adf
Rodolfo <rndcsjj@gmail.com>
2023-11-13 20:11:22 -0300

    feat: adiciona novas interface e repository

--- app/Providers/RepositoryServiceProvider.php
+++ app/Providers/RepositoryServiceProvider.php
@@ -11,9 +11,15 @@ use App\Infra\Contracts\BoletimInterface;
 use App\Infra\Contracts\DisciplinaInterface;
 use App\Infra\Contracts\NotaInterface;
 use App\Infra\Contracts\PeriodoInterface;
+use App\Infra\Contracts\PesquisadorInterface;
+use App\Infra\Contracts\PesquisadorProjetoInterface;
+use App\Infra\Contracts\ProjetoInterface;
 use App\Infra\Repositories\BoletimRepository;
 use App\Infra\Repositories\NotaRepository;
 use App\Infra\Repositories\PeriodoRepository;
+use App\Infra\Repositories\PesquisadorProjetoRepository;
+use App\Infra\Repositories\PesquisadorRepository;
+use App\Infra\Repositories\ProjetoRepository;
 use Illuminate\Support\ServiceProvider;
 
 class RepositoryServiceProvider extends ServiceProvider
@@ -29,6 +35,9 @@ class RepositoryServiceProvider extends ServiceProvider
         $this->app->bind(PeriodoInterface::class, PeriodoRepository::class);
         $this->app->bind(NotaInterface::class, NotaRepository::class);
         $this->app->bind(BoletimInterface::class, BoletimRepository::class);
+        $this->app->bind(PesquisadorInterface::class, PesquisadorRepository::class);
+        $this->app->bind(ProjetoInterface::class, ProjetoRepository::class);
+        $this->app->bind(PesquisadorProjetoInterface::class, PesquisadorProjetoRepository::class);
     }
 
     /**
